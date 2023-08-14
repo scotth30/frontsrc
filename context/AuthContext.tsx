dc45369065c15423d135522f3d18a2d011b83a58
scotth30 <scotthancock30@gmail.com>
2023-08-13 22:19:01 -0700

    Auth Update

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,8 @@
-import  { createContext, ReactNode, useEffect, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { User, onAuthStateChanged } from "firebase/auth";
 import { auth } from '../firebaseConfig'; // adjust the import to your config file path
-
+import { useNavigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 export interface AuthContextType {
   currentUser: User | null;
 }
@@ -12,25 +13,28 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate(); // Use the hook to navigate
 
-  export const AuthProvider = ({ children }: AuthProviderProps) => {
-    const [currentUser, setCurrentUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, user => {
-        setCurrentUser(user);
-        setLoading(false);
-      });
-  
-      return unsubscribe;
-    }, []);
-  
-    // If loading is true, show a loading message or spinner. Otherwise, render the children.
-    return (
-      <AuthContext.Provider value={{ currentUser }}>
-        {loading ? <div>Loading...</div> : children}
-      </AuthContext.Provider>
-    );
-  };
-  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      setLoading(false);
+
+      if (user) { // If the user is logged in, redirect to the dashboard
+        navigate('/dashboard');
+      }
+    });
+
+    return unsubscribe;
+  }, [navigate]); // Include navigate as a dependency
+
+  // If loading is true, show a loading message or spinner. Otherwise, render the children.
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {loading ? <CircularProgress /> : children}
+    </AuthContext.Provider>
+  );
+};
